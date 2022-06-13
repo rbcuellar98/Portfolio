d8c77f87a1beb2e4f05db272fed8d5d4cf562143
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-06-13 13:08:48 -0400

    style added

--- src/script.js
+++ src/script.js
@@ -203,35 +203,35 @@ window.addEventListener('mousemove', (event)=>{
 /**
  * Animate
  */
- const clock = new THREE.Clock()
- let previousTime = 0
- 
- const tick = () =>
- {
-     const elapsedTime = clock.getElapsedTime()
-     const deltaTime = elapsedTime - previousTime
-     previousTime = elapsedTime
- 
-     // Animate camera
-     camera.position.y = - scrollY / sizes.height * objectsDistance
- 
-     const parallaxX = cursor.x * 0.5
-     const parallaxY = - cursor.y * 0.5
-     cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
-     cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
- 
-     // Animate meshes
-     for(const mesh of sectionMeshes)
-     {
-         mesh.rotation.x += deltaTime * 0.1
-         mesh.rotation.y += deltaTime * 0.12
-     }
- 
-     // Render
-     renderer.render(scene, camera)
- 
-     // Call tick again on the next frame
-     window.requestAnimationFrame(tick)
- }
- 
- tick()
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+
+    const parallaxX = cursor.x * 0.5
+    const parallaxY = - cursor.y * 0.5
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
+    // Animate meshes
+    for(const mesh of sectionMeshes)
+    {
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
